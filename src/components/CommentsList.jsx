2fcb0f9eb048d790132652f60b04e5214bf845d3
adamsaunders26 <a.saunders206@gmail.com>
2023-07-18 13:51:15 +0100

    add support for articles with no comments

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,12 +1,31 @@
 import CommentCard from "./CommentCard";
 
 export default function CommentsList({ comments }) {
-  return (
-    <section className="comment-list">
-      {comments.map((comment) => {
-        return <CommentCard key={comment.comment_id} comment={comment} />;
-      })}
-      <a href="#header">Back to top</a>
-    </section>
-  );
+  if (!comments.length) {
+    console.log("here");
+    return (
+      <section>
+        <section className="comment-card">
+          <p className="no-comments">No comments yet.</p>
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+        </section>
+      </section>
+    );
+  } else {
+    return (
+      <section className="comment-list">
+        {comments.map((comment) => {
+          return <CommentCard key={comment.comment_id} comment={comment} />;
+        })}
+        <a href="#header">Back to top</a>
+      </section>
+    );
+  }
 }
